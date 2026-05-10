a17e1384d719d337f0cab4b04b8c1e192f36e3bc
Reduan-borno <arnobreduan869@gmail.com>
2026-05-10 20:12:40 +0600

    db.php

--- Model/db.php
+++ Model/db.php
@@ -3,3 +3,9 @@ class db {
     function connection() {
         $connection = new mysqli("localhost", "root", "", "hotel");
         if ($connection->connect_error) {
+            die("Connection failed: " . $connection->connect_error);
+        }
+        return $connection;
+    }
+}
+?>
