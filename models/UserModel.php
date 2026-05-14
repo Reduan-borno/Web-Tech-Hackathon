7f2d629ae0a968de434f97d9201d2481156f6ebf
Reduan-borno <arnobreduan869@gmail.com>
2026-05-14 20:47:09 +0600

    token generate and login controller set

--- models/UserModel.php
+++ models/UserModel.php
@@ -1,16 +1,18 @@
 <?php
 include_once "db.php";
 
-    function createUser($connection, $name, $email, $password, $phone, $nationality, $role) {
-    $sql = "INSERT INTO users (name, email, password_hash, phone, nationality, role) VALUES (?, ?, ?, ?, ?, ?)";
+function createUser($connection,$name,$email,$password,$phone,$nationality,$role){
+    $sql = "INSERT INTO users (name,email,password_hash,phone,nationality,role) VALUES (?,?,?,?,?,?)";
     $stmt = $connection->prepare($sql);
-    $stmt->bind_param("ssssss", $name, $email, $password, $phone, $nationality, $role);
-    if ($stmt->execute()) {
+    $stmt->bind_param("ssssss", $name, $email, $password, $phone, $nationality,$role);
+    if($stmt -> execute()){
         return true;
-    } else {
+    }
+    else{
         return false;
     }
 }
+
 function emailExists($connection, $email) {
     $sql = "SELECT id FROM users WHERE email = ?";
     $stmt = $connection->prepare($sql);
@@ -20,4 +22,26 @@ function emailExists($connection, $email) {
     return $result->num_rows > 0;
 }
 
-?>
+function loginUser($connection, $email) {
+    $sql = "SELECT id, name, role, password_hash FROM users WHERE email = ?";
+    
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    if($result->num_rows == 1){
+        return $result->fetch_assoc();
+    }
+    else{
+        return false;
+    }
+}
+
+function saveRememberToken($connection, $userId, $token){
+    $sql = "UPDATE users SET remember_token=? WHERE id=?";
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param("si", $token, $userId);
+    return $stmt->execute();
+}
