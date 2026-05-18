124a30f24464722e96ed90f3486d22df1d178b20
Arafat-2024 <23-51115-1@student.aiub.edu>
2026-05-19 02:56:11 +0600

    availableRoom php validation + Database add

--- views/bookingroom.php
+++ views/bookingroom.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+$bookingIdError = $_SESSION["bookingiderror"] ?? "";
+$bookings = $_SESSION["myBookings"] ?? [];
+
+unset($_SESSION["bookingiderror"]);
+unset($_SESSION["myBookings"]);
+?>
+
 <html>
 
 <head>
@@ -7,8 +17,10 @@
 <body>
     <h2>My Bookings</h2>
 
-    <a href="search.php">Search Room</a>
+    <a href="availableRoom.php">Search Room</a>
     <br><br>
+
+    <?php echo $bookingIdError; ?>
 
     <table border="1">
         <tr>
@@ -23,6 +35,44 @@
         </tr>
 
         <tbody id="bookingList">
+
+            <?php
+            if(count($bookings) > 0){
+                foreach($bookings as $booking){
+            ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($booking["id"]); ?></td>
+                        <td><?php echo htmlspecialchars($booking["room_type"]); ?></td>
+                        <td><?php echo htmlspecialchars($booking["room_number"]); ?></td>
+                        <td><?php echo htmlspecialchars($booking["checkin_date"]); ?></td>
+                        <td><?php echo htmlspecialchars($booking["checkout_date"]); ?></td>
+                        <td><?php echo htmlspecialchars($booking["total_price"]); ?></td>
+                        <td><?php echo htmlspecialchars($booking["status"]); ?></td>
+                        <td>
+                            <?php
+                            if($booking["status"] == "pending" || $booking["status"] == "confirmed"){
+                            ?>
+                                <form method="post" action="../Controller/cancelBooking.php">
+                                    <input type="hidden" name="booking_id" value="<?php echo htmlspecialchars($booking["id"]); ?>">
+                                    <input type="submit" value="Cancel">
+                                </form>
+                            <?php
+                            }else{
+                                echo "No Action";
+                            }
+                            ?>
+                        </td>
+                    </tr>
+            <?php
+                }
+            }else{
+            ?>
+                <tr>
+                    <td colspan="8">No booking data found</td>
+                </tr>
+            <?php
+            }
+            ?>
 
         </tbody>
     </table>
