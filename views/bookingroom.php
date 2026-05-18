b8e7a676b24463f790563d522001f9850942f7b9
Arafat-2024 <23-51115-1@student.aiub.edu>
2026-05-18 16:55:54 +0600

    My booking room page html only

---
+++ views/bookingroom.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+    <title>My Bookings</title>
+</head>
+
+<body>
+    <h2>My Bookings</h2>
+
+    <a href="search.php">Search Room</a>
+    <br><br>
+
+    <table border="1">
+        <tr>
+            <th>Booking ID</th>
+            <th>Room Type</th>
+            <th>Room Number</th>
+            <th>Check-in</th>
+            <th>Check-out</th>
+            <th>Total Price</th>
+            <th>Status</th>
+            <th>Action</th>
+        </tr>
+
+        <tbody id="bookingList">
+
+        </tbody>
+    </table>
+
+</body>
+
+</html>
