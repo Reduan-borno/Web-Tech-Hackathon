0f8504faa38ae4451b8bbda9f548d4540c525998
Arafat-2024 <23-51115-1@student.aiub.edu>
2026-05-19 02:58:57 +0600

    confirmation  php validation + Database add

--- views/confirmation.php
+++ views/confirmation.php
@@ -1,56 +1,58 @@
+<?php
+session_start();
+
+$booking_id = $_GET["booking_id"] ?? "";
+$room_type = $_GET["room_type"] ?? "";
+$checkin = $_GET["checkin"] ?? "";
+$checkout = $_GET["checkout"] ?? "";
+$total_price = $_GET["total_price"] ?? "";
+?>
+
 <html>
 <head>
     <title>Booking Confirmation</title>
-     <link rel="stylesheet" href="../assets/confrmstyle.css">
+    <link rel="stylesheet" href="../assets/confrmstyle.css">
 </head>
 <body>
     <h2>Booking Confirmation</h2>
 
-    <?php
-        $booking_id = $_GET["booking_id"] ?? "";
-        $room_type = $_GET["room_type"] ?? "";
-        $checkin = $_GET["checkin"] ?? "";
-        $checkout = $_GET["checkout"] ?? "";
-        $total_price = $_GET["total_price"] ?? "";
-    ?>
-
     <table border="1">
         <tr>
             <td>Booking ID</td>
-            <td><?php echo $booking_id; ?></td>
+            <td><?php echo htmlspecialchars($booking_id); ?></td>
         </tr>
 
         <tr>
             <td>Room Type</td>
-            <td><?php echo $room_type; ?></td>
+            <td><?php echo htmlspecialchars($room_type); ?></td>
         </tr>
 
         <tr>
             <td>Check-in Date</td>
-            <td><?php echo $checkin; ?></td>
+            <td><?php echo htmlspecialchars($checkin); ?></td>
         </tr>
 
         <tr>
             <td>Check-out Date</td>
-            <td><?php echo $checkout; ?></td>
+            <td><?php echo htmlspecialchars($checkout); ?></td>
         </tr>
 
         <tr>
             <td>Total Price</td>
-            <td><?php echo $total_price; ?></td>
+            <td><?php echo htmlspecialchars($total_price); ?></td>
         </tr>
 
         <tr>
             <td>Status</td>
-            <td>Pending</td>
+            <td>pending</td>
         </tr>
     </table>
 
     <br>
 
-    <a href="myBookings.php">Go to My Bookings</a>
+    <a href="../Controller/myBookingsController.php">Go to My Bookings</a>
     <br>
-    <a href="search.php">Search More Rooms</a>
+    <a href="availableRoom.php">Search More Rooms</a>
 
 </body>
 </html>
