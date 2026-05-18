b412d809eac9d0058b5589037ea74af7c647a9c9
Arafat-2024 <23-51115-1@student.aiub.edu>
2026-05-19 03:33:52 +0600

    avaible room page ajax add

--- controllers/availableRooms.php
+++ controllers/availableRooms.php
@@ -1,9 +1,12 @@
 <?php
+include "../Model/db.php";
+include "../Model/RoomModel.php";
 session_start();
 
 $checkin = $_GET["checkin"] ?? "";
 $checkout = $_GET["checkout"] ?? "";
 $guests = $_GET["guests"] ?? "";
+$ajax = $_GET["ajax"] ?? "";
 
 $_SESSION["checkin"] = $checkin;
 $_SESSION["checkout"] = $checkout;
@@ -27,10 +30,40 @@ if($guests == ""){
 }
 
 if($hasError){
+    if($ajax == "1"){
+        header("Content-Type: application/json");
+        echo json_encode([]);
+        exit();
+    }
+
     header("Location: ../View/availableRoom.php");
     exit();
 }else{
-    header("Location: ../View/availableRoom.php?checkin=".$checkin."&checkout=".$checkout."&guests=".$guests);
+    $db = new db();
+    $connection = $db->connection();
+
+    $availableRooms = getAvailableRoomTypes($connection, $checkin, $checkout, $guests);
+
+    $date1 = new DateTime($checkin);
+    $date2 = new DateTime($checkout);
+    $nights = $date1->diff($date2)->days;
+
+    for($i = 0; $i < count($availableRooms); $i++){
+        $availableRooms[$i]["total_price"] = $availableRooms[$i]["price_per_night"] * $nights;
+    }
+
+    if($ajax == "1"){
+        header("Content-Type: application/json");
+        echo json_encode($availableRooms);
+        exit();
+    }
+
+    $_SESSION["availableRooms"] = $availableRooms;
+    $_SESSION["checkin"] = $checkin;
+    $_SESSION["checkout"] = $checkout;
+    $_SESSION["guests"] = $guests;
+
+    header("Location: ../View/availableRoom.php");
     exit();
 }
 ?>
